3a7f1f386bb08e8cae794d0a6bac6bbe4d1e4d17
Mona Jalal <jalal@cs.wisc.edu>
2017-03-01 22:34:41 -0600

    comments fixed

--- instagram/spiders/spider.py
+++ instagram/spiders/spider.py
@@ -1,9 +1,11 @@
 import scrapy
 print(scrapy.__file__)
 import json
+import requests
 from instagram.items import UserItem
 from instagram.items import PostItem
 from scrapy.spider import BaseSpider as Spider
+from pprint import pprint
 
 class InstagramSpider(Spider):
 
@@ -12,15 +14,34 @@ class InstagramSpider(Spider):
     start_urls = []
 
     def __init__(self):
-        self.start_urls = ["https://www.instagram.com/_spataru/?__a=1"]
-
+        #self.start_urls = ["https://www.instagram.com/_spataru/?__a=1"]
+        #self.start_urls = ["https://www.instagram.com/mona_of_green_gables/?__a=1"]
+        self.start_urls = ["https://www.instagram.com/ducks_love_sun/?__a=1"]
     def parse(self, response):
         #get the json file
         json_response = {}
         try:
             json_response = json.loads(response.body_as_unicode())
+            print("monamona")
+            #print(json_response["user"]["follows"])
+            #print json.dumps(json_response["user"]["media"]["nodes"][1]["comments"][1], indent=4, sort_keys=True)
+            #print json.dumps(json_response["user"]["media"]["nodes"][1]["code"], indent=4, sort_keys=True)
+            code = json_response["user"]["media"]["nodes"][1]["code"]
+            sub_post_url = "https://www.instagram.com/p/"+code+"/?__a=1"
+            print(sub_post_url)
+            sub_response = requests.get(sub_post_url).json()
+            pprint(sub_response)
+            for i in range(len(sub_response["media"]["comments"]["nodes"])):
+                print(sub_response["media"]["comments"]["nodes"][i]["text"])
+            sub_json_data = json.loads(sub_response.body_as_unicode())
+            print("))))))((((((")
+            #pprint(sub_json_data)
+            print json.dumps(sub_json_data, indent=4, sort_keys=True)
+
+            print("*********************************")
+
         except:
-            self.logger.info('%s doesnt exist', response.url)
+            #self.logger.info('%s doesnt exist', response.url)
             pass
         if json_response["user"]["is_private"]:
             return;
@@ -29,6 +50,7 @@ class InstagramSpider(Spider):
             json_response = json_response["user"]
 
             item = UserItem()
+        
 
             #get User Info
             item["username"] = json_response["username"]
